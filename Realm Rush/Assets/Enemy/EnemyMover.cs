3536cb5d5ccc8f6a87dfbabe0aee81113c03aa03
Eisenkappler <nikolai.pronegg@gmx.at>
2023-05-06 21:52:40 +0200

    new assets, mouse down

--- Realm Rush/Assets/Enemy/EnemyMover.cs
+++ Realm Rush/Assets/Enemy/EnemyMover.cs
@@ -6,19 +6,30 @@ public class EnemyMover : MonoBehaviour
 {
 
      [SerializeField] List<Waypoint> path = new List<Waypoint>();
+     [SerializeField] [Range(0f,10f)]float speed  = 1;
 
     void Start()
     {
-        PrintWaypointName();    
+        StartCoroutine(FollowPath());    
     }
 
     
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            Vector3 startPosition =  this.transform.position;
+            Vector3 endPosition   =  waypoint.transform.position;
+            float travelPercent   = 0f;
+            transform.LookAt(endPosition);
+            while(travelPercent < 1 )
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition,endPosition,travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
+
     }
 
 }
